ffee297a277dc18ab796f7d318563db5ff59c8c3
dau2004 <c.dau@alustudent.com>
2023-11-06 00:08:21 +0200

    gg

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            result += char
-    return result
-
-if _name_ == "_main_":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
+    new_str = ""
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            new_str += i
+    return (new_str)
