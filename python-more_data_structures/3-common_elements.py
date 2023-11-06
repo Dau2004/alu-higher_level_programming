f33f867ffa98a7e09baa7043cf2c29ab61226605
dau2004 <c.dau@alustudent.com>
2023-11-06 23:04:46 +0200

    gg

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = {}
-    set_2 = {}
-    c_set = commom_elements(set_1, set_2)
-    return (sorted(list(c_set)))
+    return set_1 & set_2
