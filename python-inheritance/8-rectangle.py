70b3425b541f05f4414bc8719bc933d244c6e717
root <dau2004>
2023-11-24 13:10:28 -0800

    some

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python3
-"""
-Module for Rectangle class that inherits from BaseGeometry.
-"""
+"""Defines a Python class-to-JSON function."""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-    """
-    A class representing a Rectangle.
-    """
-    def __init__(self, width, height):
-        """
-        Initializes an instance of the Rectangle class.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """
-        Returns a string representation of the Rectangle.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
